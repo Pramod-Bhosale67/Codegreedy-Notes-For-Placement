8ab812b0d048cb8eb0166b825a5815b6e2308d52
Pramod-Bhosale67 <pramodb6716@gmail.com>
2026-03-24 20:16:14 +0530

    added the cpp code

---
+++ CPP Codes/Class Code/CPP Basics/Structure_of_CPP_Program.cpp
@@ -0,0 +1,30 @@
+
+#include<iostream>
+
+using namespace std;
+
+
+int main(){
+	
+	cout << "Hello Codegreedy";
+
+	return 0; // your program will get terminated
+}
+
+/*
+
+	1. source code (human lang code)
+
+	2. expanded source code. ----> given to the compiler
+
+	3. assembly level lang. -----> given to the assembler
+
+	4. into object file ----> will get converted into binary file
+
+	5. Linker --> linker will link all the inbuilt functions to its defination in the header file
+
+	6. loader -> it brings your executable file (binary file) from Hard disk to RAM. 
+
+
+
+*/
