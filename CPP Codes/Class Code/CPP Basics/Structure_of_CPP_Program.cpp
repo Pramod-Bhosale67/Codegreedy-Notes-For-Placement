acfc3a506a7b85a52409f14926dded1e78db0b3b
Pramod-Bhosale67 <pramodb6716@gmail.com>
2026-04-03 10:19:43 +0530

    Added the java arrays notes and industrial session PPT

--- CPP Codes/Class Code/CPP Basics/Structure_of_CPP_Program.cpp
+++ CPP Codes/Class Code/CPP Basics/Structure_of_CPP_Program.cpp
@@ -6,25 +6,8 @@ using namespace std;
 
 int main(){
 	
-	cout << "Hello Codegreedy";
 
-	return 0; // your program will get terminated
+
+	return 0; 
 }
 
-/*
-
-	1. source code (human lang code)
-
-	2. expanded source code. ----> given to the compiler
-
-	3. assembly level lang. -----> given to the assembler
-
-	4. into object file ----> will get converted into binary file
-
-	5. Linker --> linker will link all the inbuilt functions to its defination in the header file
-
-	6. loader -> it brings your executable file (binary file) from Hard disk to RAM. 
-
-
-
-*/
